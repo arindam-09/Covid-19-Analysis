48899167599b5770707d861ceb19f2cbc1f3fb3f
Arindam Talukdar <arindamt005@gmail.com>
2026-07-19 18:21:50 +0530

    Add files via upload

---
+++ SQL Queries.sql
@@ -0,0 +1,24 @@
+-- create database covid_19;
+select * from covid_19;
+
+
+-- 1. What are the top 10 countries with the highest number of confirmed COVID-19 cases?
+
+-- 2. Which 10 countries have reported the highest number of COVID-19 deaths?
+
+-- 3. What is the total number of confirmed, recovered, active, and death cases worldwide on the latest available date?
+
+-- 4. Which WHO region has the highest total number of confirmed COVID-19 cases?
+
+-- 5. Which countries have more than 100,000 confirmed COVID-19 cases?
+
+-- 6. What is the mortality rate (Deaths ÷ Confirmed × 100) for each country?
+
+-- 7. What is the recovery rate (Recovered ÷ Confirmed × 100) for each country?
+
+-- 8. Which countries have the highest number of active COVID-19 cases?
+
+-- 9. How many countries are included in each WHO region?
+
+-- 10. How did the total number of confirmed COVID-19 cases change over time (daily trend)?
+
